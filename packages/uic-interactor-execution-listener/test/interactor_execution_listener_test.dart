f0c788ce2f577d2f220e52ab33a0400a45d833a5
BuschFe <felix.busch@dataport.de>
2024-01-22 18:27:08 +0100

    Update interactor_execution_listener_test.dart

--- packages/uic-interactor-execution-listener/test/interactor_execution_listener_test.dart
+++ packages/uic-interactor-execution-listener/test/interactor_execution_listener_test.dart
@@ -13,11 +13,17 @@ class ReturnInputAfterDelayInteractor
 }
 
 void main() {
-  group(InteractorExecutionUnicastListener, () {
-    test('should notify when interactor is called', () async {
-      const interactor = ReturnInputAfterDelayInteractor();
-      final listener = InteractorExecutionUnicastListener();
+  group('run interactor with single publishTo modifier', () {
+    late ParameterizedResultInteractor<Duration, int> interactor;
 
+    setUp(() {
+      interactor = ReturnInputAfterDelayInteractor();
+    });
+
+    tearDown(() async {});
+
+    test('should emit true, false in order', () async {
+      final listener = InteractorExecutionUnicastListener();
       expectLater(listener.isLoading, emitsInOrder({true, false}));
 
       final result = await interactor(Duration(milliseconds: 50))
@@ -25,12 +31,35 @@ void main() {
           .getOrElse(fallback: -1);
 
       expect(result, 81);
+
+      await listener.release();
     });
 
-    test('should notify twice when interactor is called', () async {
-      const interactor = ReturnInputAfterDelayInteractor();
-      final listener = InteractorExecutionUnicastListener();
+    test('should emit true, false in order', () async {
+      final listener = InteractorExecutionBroadcastListener();
+      expectLater(listener.isLoading, emitsInOrder({true, false}));
 
+      final result = await interactor(Duration(milliseconds: 50))
+          .publishTo(listener)
+          .getOrElse(fallback: -1);
+
+      expect(result, 81);
+
+      await listener.release();
+    });
+  });
+
+  group('run interactor with multiple publishTo modifier', () {
+    late ParameterizedResultInteractor<Duration, int> interactor;
+
+    setUp(() {
+      interactor = ReturnInputAfterDelayInteractor();
+    });
+
+    tearDown(() async {});
+
+    test('should emit true, true, false, false in order', () async {
+      final listener = InteractorExecutionUnicastListener();
       expectLater(listener.isLoading, emitsInOrder([true, true, false, false]));
 
       final result = await interactor(Duration(milliseconds: 50))
@@ -39,27 +68,12 @@ void main() {
           .getOrElse(fallback: -1);
 
       expect(result, 81);
-    });
-  });
 
-  group(InteractorExecutionBroadcastListener, () {
-    test('should notify when interactor is called', () async {
-      const interactor = ReturnInputAfterDelayInteractor();
-      final listener = InteractorExecutionBroadcastListener();
-
-      expectLater(listener.isLoading, emitsInOrder({true, false}));
-
-      final result = await interactor(Duration(milliseconds: 50))
-          .publishTo(listener)
-          .getOrElse(fallback: -1);
-
-      expect(result, 81);
+      await listener.release();
     });
 
-    test('should notify twice when interactor is called', () async {
-      const interactor = ReturnInputAfterDelayInteractor();
+    test('should emit true, true, true, false in order', () async {
       final listener = InteractorExecutionBroadcastListener();
-
       expectLater(listener.isLoading, emitsInOrder([true, true, true, false]));
 
       final result = await interactor(Duration(milliseconds: 50))
@@ -68,6 +82,45 @@ void main() {
           .getOrElse(fallback: -1);
 
       expect(result, 81);
+
+      await listener.release();
+    });
+  });
+
+  group('run interactor with single publishInto modifier', () {
+    late ParameterizedResultInteractor<Duration, int> interactor;
+
+    setUp(() {
+      interactor = ReturnInputAfterDelayInteractor();
+    });
+
+    test('should add true, false into list', () async {
+      final states = List<bool>.empty(growable: true);
+      final result = await interactor(Duration(milliseconds: 50))
+          .publishInto((isBusy) => states.add(isBusy))
+          .getOrElse(fallback: -1);
+
+      expect(result, equals(81));
+      expect(states, equals(List.of({true, false})));
+    });
+  });
+
+  group('run interactor with multiple publishInto modifier', () {
+    late ParameterizedResultInteractor<Duration, int> interactor;
+
+    setUp(() {
+      interactor = ReturnInputAfterDelayInteractor();
+    });
+
+    test('should add true, true, true, false into list', () async {
+      final states = List<bool>.empty(growable: true);
+      final result = await interactor(Duration(milliseconds: 50))
+          .publishInto((isBusy) => states.add(isBusy))
+          .publishInto((isBusy) => states.add(isBusy))
+          .getOrElse(fallback: -1);
+
+      expect(result, equals(81));
+      expect(states, equals([true, true, false, false]));
     });
   });
 }
