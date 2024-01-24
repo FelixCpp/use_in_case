159bb2162f0e88b04d9759cd63a9baec411911f9
BuschFe <felix.busch@dataport.de>
2024-01-24 12:17:07 +0100

    Examples written for the timeout + busy-state extensions

--- packages/uic-interactor-busy-state/example/uic_interactor_busy_state_example.dart
+++ packages/uic-interactor-busy-state/example/uic_interactor_busy_state_example.dart
@@ -1,1 +1,88 @@
+import 'package:uic_interactor/uic_interactor.dart';
+import 'package:uic_interactor_busy_state/uic_interactor_busy_state.dart';
 
+class WaitingInteractor extends ParameterizedResultInteractor<Duration, int> {
+  const WaitingInteractor();
+
+  @override
+  Future<int> execute(Duration delay) {
+    return Future.delayed(delay, () => 99);
+  }
+}
+
+Future<void> exampleWithReceiver() async {
+  const interactor = WaitingInteractor();
+  final stopwatch = Stopwatch();
+
+  final result = await interactor(
+    const Duration(milliseconds: 50),
+  ).receiveBusyStateChange((isBusy) {
+    if (isBusy) {
+      stopwatch.start();
+    } else {
+      stopwatch.stop();
+    }
+  }).get();
+
+  final elapsedTime = stopwatch.elapsed;
+  print('Task took $elapsedTime and returned $result');
+}
+
+Future<void> exampleWithConsumingListener() async {
+  const interactor = WaitingInteractor();
+  final listener = BusyStateConsumingListener();
+  final stopwatch = Stopwatch();
+
+  final subscription = listener.isLoading.listen((isBusy) {
+    if (isBusy) {
+      stopwatch.start();
+    } else {
+      stopwatch.stop();
+    }
+  });
+
+  final result = await interactor(
+    const Duration(milliseconds: 50),
+  ).listenOnBusyState(listener).get();
+
+  final elapsedTime = stopwatch.elapsed;
+  print('Task took $elapsedTime and returned $result');
+
+  await subscription.cancel();
+  await listener.release();
+}
+
+Future<void> exampleWithBufferingListener() async {
+  const interactor = WaitingInteractor();
+  final listener = BusyStateBufferingListener();
+  final stopwatch = Stopwatch();
+
+  final subscription = listener.isLoading.listen((isBusy) {
+    if (isBusy) {
+      stopwatch.start();
+    } else {
+      stopwatch.stop();
+    }
+  });
+
+  final results = await Future.wait({
+    interactor(const Duration(milliseconds: 50))
+        .listenOnBusyState(listener)
+        .get(),
+    interactor(const Duration(milliseconds: 100))
+        .listenOnBusyState(listener)
+        .get()
+  });
+
+  final elapsedTime = stopwatch.elapsed;
+  print('Task took $elapsedTime and returned $results');
+
+  await subscription.cancel();
+  await listener.release();
+}
+
+Future<void> main() async {
+  await exampleWithReceiver();
+  await exampleWithConsumingListener();
+  await exampleWithBufferingListener();
+}
--- packages/uic-interactor-timeout/example/uic_interactor_timeout_example.dart
+++ packages/uic-interactor-timeout/example/uic_interactor_timeout_example.dart
@@ -1,1 +1,21 @@
+import 'package:uic_interactor/uic_interactor.dart';
+import 'package:uic_interactor_timeout/src/timeout_extension.dart';
 
+class WaitingInteractor extends ResultInteractor<int> {
+  const WaitingInteractor();
+
+  @override
+  Future<int> execute(Nothing _) {
+    return Future.delayed(Duration(milliseconds: 50), () => 99);
+  }
+}
+
+Future<void> main() async {
+  const interactor = WaitingInteractor();
+
+  final result = await interactor(nothing)
+      .timeout(Duration(milliseconds: 25))
+      .getOrElse(fallback: -1);
+
+  print(result);
+}
